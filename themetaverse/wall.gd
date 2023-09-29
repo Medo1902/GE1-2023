109eb61d670e5b3510db7639ef514fa0e6ba5118
C20338661 Ivan Medved <c20338661@mytudublin.ie>
2023-09-29 15:39:52 +0100

    1st commit

--- themetaverse/wall.gd
+++ themetaverse/wall.gd
@@ -4,10 +4,13 @@ extends Node3D
 
 # Declare member variables here. Examples:
 
-@export var width = 10
-@export var height = 10
+@export var width = 10 # variance type, in this example are numeric, int or float
+@export var height = 10 # @export, exposes it in godot editor (inspector)
 
-@export var brickScene:PackedScene
+@export var brickScene:PackedScene # premade scene, reference another scene already created
+
+func _process(delta):
+	print(delta)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,16 +20,16 @@ func _ready():
 	for j in height:
 		for i in width:				
 			var brick = brickScene.instantiate()
-			var s = 1			
-			var pos = Vector3((i - half) * s , 1 + (j * s), 0)
-			brick.transform.origin = pos
+			var s = 0.5			
+			var pos = Vector3((i - half) * s , 1 + (j * s), 0) # calculating position
+			brick.transform.origin = pos # using transform of the bring (position, rotation, scale)
 			
-			var newMaterial = StandardMaterial3D.new()
+			var newMaterial = StandardMaterial3D.new() # creating new material
 			
-			newMaterial.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8)
+			newMaterial.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8) # setting random coloour
 			brick.get_node("MeshInstance3D").material_override = newMaterial
 			
-			add_child(brick)
+			add_child(brick) # adds it to a scene tree
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
